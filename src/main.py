060c441ac01c54617078c5cb1ecb0c4e6abb97bb
Kirill-max-SR <122700677+Kirill-max-SR@users.noreply.github.com>
2023-01-17 18:25:06 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import modules.perimeter as perimeter
+import modules.factorial as factorial
+f1=factorial.factorial_norm(5)
+print(f1)
 p1=perimeter.perimeter_func([1,2,5])
 print(p1)
 p2=perimeter.perimeter_func([1,2,5,7,1])
