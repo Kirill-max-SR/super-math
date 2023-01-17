137fa2addfd7eddda672d3333e5e420c1303a31e
Nick <82q0yjzgkdfp@mail.ru>
2023-01-17 17:47:14 +0300

    Сommit 5

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import modules.perimeter as perimeter
-p1=perimetere.perimeter_func([1,2,5])
+p1=perimeter.perimeter_func([1,2,5])
 print(p1)
-p2=perimetere.perimeter_func([1,2,5,7,1])
+p2=perimeter.perimeter_func([1,2,5,7,1])
 print(p2)
 print('Hello World')
