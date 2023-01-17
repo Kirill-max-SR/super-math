82f51735310a84965bf6b9f906ac07d4a14b8981
Nick <82q0yjzgkdfp@mail.ru>
2023-01-17 22:44:39 +0300

    Commit 7

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import modules.perimeter as perimeter
 import modules.factorial as factorial
+import modules.test as test
+import modules.tests.test_all as ta
 f1=factorial.factorial_res(5)
 print(f1)
 p1=perimeter.perimeter_func([1,2,5])
